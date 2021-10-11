ab9384a22e95772c03c83d38f2a90bf69e073179
Gabriel <shalitmontagne.g@gmail.com>
2021-10-10 22:08:42 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+/*
 const titleCased = () => {
   return tutorials
 }
+*/
+
+
+function titleCased() {
+  return tutorials.map(function(sentence) {
+    return sentence.split(' ').map(function(word) {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ');
+  });
+};
